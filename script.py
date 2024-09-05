5bbb8fd8281e15f44a5c52feff46fbc4ab11b91e
ocamposfaria <ocamposfaria@gmail.com>
2024-09-05 14:00:38 -0300

    building...

--- script.py
+++ script.py
@@ -28,7 +28,8 @@ def is_system_event(message):
         "mudou a descrição do grupo", 
         "mudou a imagem do grupo",
         "são protegidas com a criptografia de ponta a ponta",
-        "foi adicionado(a)"
+        "foi adicionado(a)",
+        "‎"
     ]
     return any(event in message.lower() for event in system_events)
 
